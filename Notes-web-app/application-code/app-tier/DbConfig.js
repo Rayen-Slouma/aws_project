7a87d9a7ca899784204a23f9ec1d4c766b1fc43d
Rayen Slouma <115884628+Rayen-Slouma@users.noreply.github.com>
2025-01-29 22:43:39 +0100

    Update DbConfig.js

--- Notes-web-app/application-code/app-tier/DbConfig.js
+++ Notes-web-app/application-code/app-tier/DbConfig.js
@@ -1,5 +1,5 @@
 module.exports = Object.freeze({
-    DB_HOST : 'projetdb.cv3zdldjrqyr.us-east-1.rds.amazonaws.com',
+    DB_HOST : 'projectdb.czrfpomajxkb.us-east-1.rds.amazonaws.com',
     DB_USER : 'admin',
     DB_PWD : 'admin',
     DB_DATABASE : 'webappdb'
